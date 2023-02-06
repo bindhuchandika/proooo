92b026bcb59b8fedec22dac3edc67ac255af714c
bindhuchandika <bindhuchandika@gmail.com>
2023-02-06 17:05:01 +0530

    shobhamam

--- Test/src/test/java/script/Sampleee.java
+++ Test/src/test/java/script/Sampleee.java
@@ -10,29 +10,18 @@ package script;
 	import io.github.bonigarcia.wdm.WebDriverManager;
 	public class Sampleee
 	{
-	WebDriver driver;
-	@BeforeMethod
-	public void start()
-	{
+		@Test
+		public void m(){
 			   WebDriverManager.chromedriver().setup();
-			   driver = new ChromeDriver();
+			   WebDriver driver = new ChromeDriver();
 			   driver.get("http://localhost:8888/");
-			   driver.manage().window().maximize();
-	}	
-	@Test
-	public void testcase()
-	{
-	           driver.findElement(By.name("user_name")).sendKeys("admin");
+			   driver.findElement(By.name("user_name")).sendKeys("admin");
 			   driver.findElement(By.name("user_password")).sendKeys("admin");
 			   driver.findElement(By.id("submitButton")).click();
 			   driver.findElement(By.xpath("//a[text()='Contacts']")).click();
 			   driver.findElement(By.xpath("//img[@alt='Create Contact...']")).click();
-	}
-	@AfterMethod
-	public void end()
-	{
-		driver.findElement(By.xpath("(//td[@class='small'])[2]")).click();
-	    driver.findElement(By.linkText("Sign Out")).click();
-	}
-	}
+			   driver.findElement(By.xpath("(//td[@class='small'])[2]")).click();
+		       driver.findElement(By.linkText("Sign Out")).click();
 
+		}
+	}
